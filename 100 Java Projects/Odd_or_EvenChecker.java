b32bc528858b1d46e79262b61cc3e765f8800a31
Kazu <kazuuchiha69@gmail.com>
2025-07-23 10:50:23 +0800

    odd or evenchecker

---
+++ 100 Java Projects/Odd_or_EvenChecker.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Odd_or_EvenChecker {
+    public static void main(String[] args) {
+        Scanner w = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = w.nextInt();
+
+        if(number % 2 == 0){System.out.println("Even number");}
+        else{System.out.println("Odd Number");}
+    }
+}
