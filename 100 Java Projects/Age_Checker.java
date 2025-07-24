fd411f7fbb8c060922057f416a8680eca33e3e5f
Kazu <kazuuchiha69@gmail.com>
2025-07-24 19:11:38 +0800

    update

---
+++ 100 Java Projects/Age_Checker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Age_Checker {
+    public static void main(String[] args) {
+        Scanner w = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        if(w.hasNextInt()){
+            int age = w.nextInt();
+
+            if(age > 17){System.out.println("Access granted. You are " + age + " or older.");}
+            else{System.out.println("Access denied. You must be " + age + " or older.");}
+ 
+        }else{System.out.println("That's not a valid number!");}
+        w.close();
+        
+    }
+}
