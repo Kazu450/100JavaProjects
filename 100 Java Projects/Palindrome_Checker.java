6c8d1b4ec3baaf66849bfbaf95854878f277a2a4
Kazu <kazuuchiha69@gmail.com>
2025-07-26 17:49:03 +0800

    update

---
+++ 100 Java Projects/Palindrome_Checker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Palindrome_Checker {
+
+    public static void main(String[] args) {
+        Scanner w = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = w.nextLine();
+        String reversed = new StringBuilder(word).reverse().toString();
+        System.out.println(reversed);
+
+        if(word.equalsIgnoreCase(reversed)){System.out.println(word + " is a palindrome");}
+        else{System.out.println(word + " is not a palindrome");}
+
+    }
+    
+}
